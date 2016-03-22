0df9dc3089b443c0ae111438220e75d65d2700ef
Kiran Tambe <kiran.tambe@gmail.com>
2016-03-22 23:48:02 +0530

    added index template.

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
 # -*- coding: utf-8 -*-
     
-from flask import Flask
-from flask import Response
-from flask import stream_with_context
+from flask import Flask, Response, render_template, stream_with_context
 
 import requests
 
 app = Flask(__name__)
 
 @app.route('/<path:url>')
-def home(url):
-    req = requests.get(url, stream = True)
-    return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
+def kprx(url):
+	if not url.startswith('https://') and not url.startswith('https://'):
+		url = 'http://' + url
+	req = requests.get(url, stream=True)
+	return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
+
+@app.route('/')
+def index():
+	return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run()
+	app.run(debug=True)
