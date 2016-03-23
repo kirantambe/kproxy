6458039215e57814fed9b378686152d73283e0d8
Kiran Tambe <kiran.tambe@hdfcred.com>
2016-03-23 10:42:03 +0530

    added post method.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
     
-from flask import Flask, Response, render_template, stream_with_context
+from flask import Flask, Response, render_template, stream_with_context, request
 
 import requests
 
@@ -13,8 +13,13 @@ def kprx(url):
 	req = requests.get(url, stream=True)
 	return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+	if request.method == "POST":
+		url = request.form['inpurl']
+		req = requests.get(url, stream=True)
+		return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
 	return render_template('index.html')
 
 if __name__ == '__main__':
