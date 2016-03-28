635e5d8e7e22f984d019cdb469c1700f200b4fea
Kiran Tambe <kiran.tambe@hdfcred.com>
2016-03-28 14:09:32 +0530

    minor fix.

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import requests
 app = Flask(__name__)
 
 def generate_response(url):
-	if not url.startswith('https://') and not url.startswith('https://'):
+	if not url.startswith('https://') and not url.startswith('http://'):
 		url = 'http://' + url
 	req = requests.get(url, stream=True)
 	return Response(stream_with_context(req.iter_content()), content_type = req.headers['content-type'])
